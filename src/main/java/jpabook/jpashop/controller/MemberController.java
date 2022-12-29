e30e4d08ca1d9ac014868bbb6fe26cfa30e4e0fd
Jinwoo Park <zvzv9808@gmail.com>
2022-12-29 23:32:02 +0900

    create UI with Bootstrap & create Member Join form

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    /**
+     * @Valid: @NotEmpty 기능의 사용
+     */
+    //Member Entity를 사용하지 않는 이유
+    //1. @NotEmpty등 Validation을 쓰게 된다면 Entity가 지저분해짐.
+    //2. 컨트롤러에서 넘어오는 Validation과 실제 도메인이 원하는 Validation이 다를 수 있음.
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+
+        if (result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
